04f7ae888314427ffa064047d60013483465ee28
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-09-04 10:14:59 -0300

    async prototype save

--- public/js/hcode-formsave.js
+++ public/js/hcode-formsave.js
@@ -4,9 +4,33 @@ HTMLFormElement.prototype.save = function () {
     console.log(form.action)
     console.log('formmm', form)
     return new Promise((resolve, reject) => {
-        form.addEventListener('submit', e => {
-            e.preventDefault();
-            let formData = new FormData(form)
+        form.addEventListener('submit', e => {HTMLFormElement.prototype.save = function () {
+            let form = this;
+            console.log('form')
+            console.log(form.action)
+            console.log('formmm', form)
+            return new Promise((resolve, reject) => {
+                form.addEventListener('submit', e => {
+                    e.preventDefault();
+                    let formData = new FormData(form)
+                    fetch(form.action, {
+                        method: form.method,
+                        body: formData
+                    })
+                        .then(response => {
+                            console.log('chegou em response:')
+                            console.log(response)
+                            response.json()
+                        })
+                        .then(json => {
+                            resolve(json)
+                        }).catch(err => {
+                            console.log('Erro:', err)
+                            reject(err);
+                        })
+                })
+            })
+        }
             fetch(form.action, {
                 method: form.method,
                 body: formData
